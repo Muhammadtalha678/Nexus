65da7030e234cf1794cb2abcf09b8aa5441a1cf4
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-12 00:59:15 +0500

    login controller complete

--- backend_express/src/routers/auth.router.js
+++ backend_express/src/routers/auth.router.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { validate_request } from '../middlewares/validate.middleware.js'
-import {register_validation} from '../lib/validations/auth.validation.js'
-import { register_controller, verify_email_controller } from '../controllers/auth.controller.js'
+import {login_validation, register_validation} from '../lib/validations/auth.validation.js'
+import { login_controller, register_controller, verify_email_controller } from '../controllers/auth.controller.js'
 const router = express.Router()
 
 router.post('/register',validate_request(register_validation),register_controller)
 router.post('/verify-email',verify_email_controller)
+router.post('/login',validate_request(login_validation),login_controller)
 
 export default router
