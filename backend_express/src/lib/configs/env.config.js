a32332352c2c5a112da9c6022c2419c8b54699ca
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-12 00:01:03 +0500

    add email configs

--- backend_express/src/lib/configs/env.config.js
+++ backend_express/src/lib/configs/env.config.js
@@ -2,5 +2,9 @@ import "dotenv/config";
 
 export const env_config = {
     port:process.env.PORT,
-    mongo_uri:process.env.MONGO_URI,
+    mongo_uri: process.env.MONGO_URI,
+    Email_HOST : process.env.EMAIL_HOST,
+    EMAIL_PORT : process.env.EMAIL_PORT,
+    EMAIL_USER : process.env.EMAIL_USER,
+    EMAIL_PASS : process.env.EMAIL_PASS
 }
