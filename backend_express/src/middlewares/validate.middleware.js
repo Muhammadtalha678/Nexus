c755f305c38b8cff737c5bbde2acb3b34cd6ae3e
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-11 20:58:26 +0500

    .

--- backend_express/src/middlewares/validate.middleware.js
+++ backend_express/src/middlewares/validate.middleware.js
@@ -2,7 +2,7 @@ import { sendResponse } from "../lib/helpers/sendResponse.js";
 
 export const validate_request = (validationSchema) => (req, res, next) => {
     const { error } = validationSchema.validate(req.body, { abortEarly: false })//means sb aik sath validate
-    print("error joi" ,error)
+    // console.log("error joi" ,error)
     if (error) {
         // Transform Joi error details to field-wise messages
         const fieldErrors = {};
