81a754488bf8227efee5a56ecbfbb2fa6b537d71
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-11 21:28:49 +0500

    add verification token

--- backend_express/src/models/user.model.js
+++ backend_express/src/models/user.model.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose'
 
-const UserSchema = new  mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['entrepreneur', 'investor'], required: true },
     isVerified: { type: Boolean, default: false },
     otpExpiresAt: { type: Date, required: true },
+    verificationToken: { type: String },
 },
-{timestamps:true}
+    {timestamps:true}
 )
 
 const UserModel = mongoose.model('users', UserSchema)
