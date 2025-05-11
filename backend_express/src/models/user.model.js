a8ab1c15f3ddafec41a20dd2b829fd30b89021b0
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-11 21:00:09 +0500

    add role email expires time after register to verify

--- backend_express/src/models/user.model.js
+++ backend_express/src/models/user.model.js
@@ -3,7 +3,10 @@ import mongoose from 'mongoose'
 const UserSchema = new  mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    role: { type: String, enum: ['entrepreneur', 'investor'], required: true },
+    isVerified: { type: Boolean, default: false },
+    otpExpiresAt: { type: Date, required: true },
 },
 {timestamps:true}
 )
