a3a3d6d8f2db750d55b416d9c9c42d8ef1a49395
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-12 00:58:13 +0500

    .

--- backend_express/src/controllers/auth.controller.js
+++ backend_express/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt'
 import { sendResponse } from '../lib/helpers/sendResponse.js'
 import UserModel from '../models/user.model.js'
+import {send_verification_mail} from '../lib/helpers/mailerService.js'
+import {generateAccessToken} from '../lib/tokens/generate.token.js'
 const register_controller = async (req, res) => {
     try {
         let { name, email, password,role } = req.body
@@ -18,6 +20,14 @@ const register_controller = async (req, res) => {
             name,email,password,role,otpExpiresAt,verificationToken
         })
 
+        try {
+            await send_verification_mail(email,verificationToken)
+        } catch (error) {
+            // Rollback user creation if email sending fails
+                        // await UserModal.findByIdAndDelete(newUSer._id)
+                        return sendResponse(res, 500, true, { general: "Failed to send verification email. Please try again." }, null);
+        }
+
         await new_user.save()
         return sendResponse(res,200,false,{},{email:new_user.email,name:new_user.name,message:"User Registered Succesfully, OTP sent to email."})
 
@@ -26,4 +36,48 @@ const register_controller = async (req, res) => {
     }
 }
 
-export {register_controller}
+const verify_email_controller = async (req, res) => {
+    try {
+        let {email,token} = req.body
+
+        const find_user = await UserModel.findOne({ email })
+        if (!find_user) {
+            return sendResponse(res,401,true,{email:"User not found"},null)
+        }
+
+        // if (find_user.otpExpiresAt < Date.now()) throw new Error("OTP has expired");
+        if (find_user.otpExpiresAt < Date.now()) return sendResponse(res, 400, true, { general: "OTP has expired" }, null);
+        
+        if (find_user.verificationToken !== token) return sendResponse(res, 400, true, {otp:"Invalid OTP"}, null); 
+        
+        find_user.isVerified = true
+        find_user.verificationToken = undefined
+        await find_user.save()
+        return sendResponse(res, 200, false, {}, {message:"OTP verified successfully"});
+
+
+    } catch (error) {
+         return sendResponse(res,500,true,{ general: error.message },null)
+    }
+}
+
+const login_controller = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await UserModel.findOne({ email })
+        if (!user) return sendResponse(res, 409, true, { email: "User is not registered" }, null)
+        
+        if (!user.isVerified) {
+                    return sendResponse(res,409,true,{isVerified:user.isVerified,otpExpiresAt:user.otpExpiresAt,general:"Please verify your email before logging in."},null)
+                    
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if (!isPasswordValid) return sendResponse(res, 401, true, { email: "Invalid email or password" }, null);
+        const accessToken = generateAccessToken({id:user._id,email:user.email,role:user.role})
+         return sendResponse(res,200,false,{},{email:user.email,role:user.role,accessToken,message:"User Login Successfully"})
+        
+    } catch (error) {
+        return sendResponse(res,500,true,{ general: error.message },null)
+    }
+}
+export {register_controller,verify_email_controller,login_controller}
