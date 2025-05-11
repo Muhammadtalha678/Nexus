0a5f8ef70d6e86232cbccdce59b15d62b8ddfc65
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-12 00:01:27 +0500

    add verify email router path

--- backend_express/src/routers/auth.router.js
+++ backend_express/src/routers/auth.router.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { validate_request } from '../middlewares/validate.middleware.js'
 import {register_validation} from '../lib/validations/auth.validation.js'
-import { register_controller } from '../controllers/auth.controller.js'
+import { register_controller, verify_email_controller } from '../controllers/auth.controller.js'
 const router = express.Router()
 
 router.post('/register',validate_request(register_validation),register_controller)
+router.post('/verify-email',verify_email_controller)
 
 export default router
