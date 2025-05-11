c934e1128f4556a37e3f995be8aaa0330b11d89a
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-11 15:57:10 +0500

    add joi validation for register user

---
+++ backend_express/src/lib/validations/auth.validation.js
@@ -0,0 +1,40 @@
+import joi from 'joi'
+
+const register_validation = joi.object({
+    name: joi.string().min(3).max(255).required()
+        .messages({
+                'string.empty': 'Full name is required',
+                'string.min': 'Full name must be at least 3 characters',
+                'any.required': 'Full name is required',
+            }),
+        email: joi.string().email({ minDomainSegments: 2, tlds: {allow:['com', 'net']} }).required()
+        .messages({
+                'string.empty': 'Email is required',
+                'string.email': 'Please provide a valid email address',
+                'any.required': 'Email is required',
+            }),
+        password: joi.string().min(8).required()
+        .messages({
+                'string.empty': 'Password is required',
+                'string.min': 'Password must be at least 8 characters',
+                'any.required': 'Password is required',
+            }),
+})
+
+const login_validation = joi.object({
+    email: joi.string().email({ minDomainSegments: 2, tlds: {allow:['com', 'net']} }).required()
+    .messages({
+            'string.empty': 'Email is required',
+            'string.email': 'Please provide a valid email address',
+            'any.required': 'Email is required',
+        }),
+    password: joi.string().min(8).required()
+    .messages({
+            'string.empty': 'Password is required',
+            'string.min': 'Password must be at least 8 characters',
+            'any.required': 'Password is required',
+        }),
+
+})
+
+export {register_validation,login_validation}
