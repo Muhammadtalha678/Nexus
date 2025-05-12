73e838539d4155af2abea5843eda161fcecf6093
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-12 17:14:27 +0500

    add socket io

---
+++ backend_express/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { connectDb } from "./lib/db/connectDb.js";
+import { env_config } from "./lib/configs/env.config.js";
+import AuthRouter from './routers/auth.router.js'
+import { Server } from 'socket.io'
+import http from 'http'
+import cors from 'cors'
+const app = express()
+app.get('/', (req,res) => {
+    return res.send("Hello World")
+})
+
+// app.use(cors({
+//     origin:['http://127.0.0.1:5500']
+// }))
+app.use(express.json())
+
+app.use('/api/auth',AuthRouter)
+
+export default app
--- backend_express/src/index.js
+++ backend_express/src/index.js
@@ -1,25 +1,53 @@
-import express from "express";
-import { connectDb } from "./lib/db/connectDb.js";
-import { env_config } from "./lib/configs/env.config.js";
-import AuthRouter from './routers/auth.router.js'
-const app = express()
+import app from './app.js'
+import {Server} from 'socket.io'
+import http from 'http'
+import { connectDb } from './lib/db/connectDb.js'
+import { env_config } from './lib/configs/env.config.js'
 
-app.use(express.json())
+const server = http.createServer(app)
 
-app.get('/', (req,res) => {
-    return res.send("Hello World")
+const io = new Server(server
+  , {
+  cors: {
+    origin: '*', // frontend se allow
+    // methods: ['GET', 'POST']
+  }
+}
+)
+
+io.on('connection', (socket) => {
+  console.log('🟢 User connected:', socket.id);
+  // io.emit('newMessage', data); //// Send to all
+
+  socket.emit('yourSocketId', socket.id); // return the id of socket to user who connected
+  
+  //  // Send to all others 
+  // socket.broadcast.emit('newMessage', data); 
+  
+  socket.on('sendMessage', ({ to, from, message }) => {
+    console.log(`📩 Message from ${from} to ${to}:`, message);
+    io.to(to).emit("newMessage", { from, message });
+  });
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 })
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    server.close(() => {
+      console.log('Closed remaining connections gracefully.');
+      process.exit(1); 
+    });
+  });
 
 connectDb()
     .then(() => {
-        app.use('/api/auth',AuthRouter)
-
-        app.listen(env_config.port, () => {
-            console.log("app running on port "+env_config.port);
-            
+        server.listen(env_config.port, () => {
+            console.log("app running on port "+env_config.port);              
         })
-    }).
-    catch((e) => {
-        console.log(e);
     })
-
+    .catch((e) => {
+        console.log("Failed to connect DB", error);
+        
+    })
