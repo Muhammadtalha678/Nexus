7d560d3e755f4fab2ae1e981f4cfce0ad244c2ae
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-13 19:17:51 +0500

    add verify email route

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -8,12 +8,14 @@ import Login from './pages/login'
 import DashboardLayout from './components/layouts/DashboardLayout'
 import InvestorDashboard from './pages/dashboard/investor'
 import EntrepreneurDashboard from './pages/dashboard/entrepreneur'
+import VerifyEmail from './pages/verify-email'
 
 function App() {
   return (
     <Routes>
       <Route path='/register' element={<Register/>} />
       <Route path='/login' element={<Login />} />
+      <Route path='/verify-email' element={<VerifyEmail />} />
       <Route element={<DashboardLayout />}>
         
        <Route path='/dashboard/investor' element={<InvestorDashboard/>} />
