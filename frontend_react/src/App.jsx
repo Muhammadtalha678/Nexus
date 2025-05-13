8616d8d73577f7d4ba4038b6ba70f02dd1ce3edc
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-13 15:41:22 +0500

    create routes for dashboard layout and layout for investor and enterprenour

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -5,12 +5,20 @@ import './App.css'
 import { Route, Routes } from 'react-router'
 import Register from './pages/register'
 import Login from './pages/login'
+import DashboardLayout from './components/layouts/DashboardLayout'
+import InvestorDashboard from './pages/dashboard/investor'
+import EntrepreneurDashboard from './pages/dashboard/entrepreneur'
 
 function App() {
   return (
     <Routes>
       <Route path='/register' element={<Register/>} />
-      <Route path='/login' element={<Login/>} />
+      <Route path='/login' element={<Login />} />
+      <Route element={<DashboardLayout />}>
+        
+       <Route path='/dashboard/investor' element={<InvestorDashboard/>} />
+       <Route path='/dashboard/entrepreneur' element={<EntrepreneurDashboard/>} />
+      </Route>
    </Routes>
   )
 }
