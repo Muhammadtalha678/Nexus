225a9432e5d91966697641ec997c970ccc15bed3
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-14 20:43:42 +0500

    .

--- backend_express/src/controllers/business.controller.js
+++ backend_express/src/controllers/business.controller.js
@@ -4,24 +4,43 @@ import UserModel from "../models/user.model.js"
 const entrepreneurs_controller = async (req, res) => {
     try {
         const { role } = req.user
-        console.log(role);
+        console.log('investor',role);
         
         if (role !== 'investor') {
             return sendResponse(res, 400, true, { general: "Invalid user role" }, null)
         }
-        const allEntrepreneurs = await UserModel.find({ role: 'investor' })
-        return sendResponse(res, 200, false, null, {allEntrepreneurs })
+        let allEntrepreneur = await UserModel.find({ role: 'entrepreneur' })
+        allEntrepreneur = allEntrepreneur.map((entrepreneur) => {
+            return {
+                role:entrepreneur.role,
+                name: entrepreneur.name,
+                company: entrepreneur.organization,
+                pitchSummary:entrepreneur.pitchSummary || entrepreneur.bio || "Experienced investor looking for next opportunity"
+            }
+        })
+        return sendResponse(res, 200, false, null, {allEntrepreneur })
     } catch (error) {
-        
+        sendResponse(res, 500, true, {general:`Something went wrong ${error}`},null)
+
     }
 }
 const investors_controller = async (req, res) => {
     try {
         const { role } = req.user
+        console.log("entrepreneur",role);
+        
         if (role !== 'entrepreneur') {
             return sendResponse(res, 400, true, { general: "Invalid user role" }, null)
         }
-        const allInvestors = await UserModel.find({ role: 'entrepreneur' })
+        let allInvestors = await UserModel.find({ role: 'investor' })
+        allInvestors = allInvestors.map((investor) => {
+            return {
+                role:investor.role,
+                name: investor.name,
+                startup:investor.startupName,
+                pitchSummary:investor.pitchSummary,
+            }
+        })
         return sendResponse(res, 200, false, null, {allInvestors })
         
     } catch (error) {
