8792f73b6e6639a9270c3e8a15bf2401bd59fddb
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-14 06:34:31 +0500

    add  addtional fields for Entrepreneurs and Investors

--- backend_express/src/models/user.model.js
+++ backend_express/src/models/user.model.js
@@ -8,6 +8,21 @@ const UserSchema = new mongoose.Schema({
     isVerified: { type: Boolean, default: false },
     otpExpiresAt: { type: Date, required: true },
     verificationToken: { type: String },
+   
+    bio: { type: String },// Short professional bio (shared)
+
+    // Fields specific to Entrepreneurs
+    startupName: { type: String },   // Name of the startup
+    startupDescription: { type: String },   // Full description of startup
+    pitchSummary: { type: String },   // Short pitch (shown on dashboard cards)
+    fundingGoal: { type: String },   // Target funding amount (e.g., "$500K")
+    pitchDeckUrl: { type: String },   // URL to pitch deck (PDF, Slide, etc.)
+
+     // Fields specific to Investors
+     organization: { type: String },   // Name of the investor’s company/org
+     portfolioSize: { type: String },   // Size of portfolio (e.g., "$5M")
+     interests: [{ type: String }],  // Investment interests (e.g., ["HealthTech", "AI"])
+     portfolioCompanies: [{ type: String }],  // Optional: names of companies they’ve backed
 },
     {timestamps:true}
 )
