163e906f44780ba747842cbd9501e916fb84efff
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-14 05:49:10 +0500

    authenticate user according to role and token

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,26 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Route, Routes } from 'react-router'
+import { Route, Routes,Navigate } from 'react-router'
 import Register from './pages/register'
 import Login from './pages/login'
 import DashboardLayout from './components/layouts/DashboardLayout'
 import InvestorDashboard from './pages/dashboard/investor'
 import EntrepreneurDashboard from './pages/dashboard/entrepreneur'
 import VerifyEmail from './pages/verify-email'
-
+import {useAuth} from './context/Authcontext'
+import NotFound from './components/not-found'
 function App() {
+  const {user} = useAuth()
+  // console.log(user);
+  const getDashboardRedirect = () => {
+    if (!user) return <Navigate to="/login" />;
+    return <Navigate to={user.role === 'investor' ? '/dashboard/investor' : '/dashboard/entrepreneur'} />;
+  };
+
   return (
     <Routes>
-      <Route path='/register' element={<Register/>} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/verify-email' element={<VerifyEmail />} />
-      <Route element={<DashboardLayout />}>
+      <Route path='/register' element={!user ? <Register/> :getDashboardRedirect()} />
+      <Route path='/login' element={!user ? <Login /> :  getDashboardRedirect()}/>
+      <Route path='/verify-email' element={!user? <VerifyEmail />: getDashboardRedirect()} />
+       {/* Protected Dashboard Routes */}
+      <Route element={user? <DashboardLayout /> : <Navigate to={'/login'}/>}>
+        <Route path="/dashboard" element={getDashboardRedirect()} />
         
-       <Route path='/dashboard/investor' element={<InvestorDashboard/>} />
-       <Route path='/dashboard/entrepreneur' element={<EntrepreneurDashboard/>} />
+       <Route path="/dashboard/investor" element={user?.role === 'investor' ? <InvestorDashboard /> : <Navigate to="/dashboard" />} />
+       <Route path="/dashboard/entrepreneur" element={user?.role === 'entrepreneur' ? <EntrepreneurDashboard /> : <Navigate to="/dashboard" />} />
       </Route>
+      <Route path='*' element={<NotFound/>} />
    </Routes>
   )
 }
