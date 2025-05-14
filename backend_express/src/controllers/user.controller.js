58c8d625e034502efc35ed6a8bfdd5fdcf41236c
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-14 08:00:43 +0500

    add profile update user route

--- backend_express/src/controllers/user.controller.js
+++ backend_express/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { sendResponse } from "../lib/helpers/sendResponse.js"
 
-export const user_controller = async (req, res) => {
+const user_controller = async (req, res) => {
     try {
         const {name,email,role} = req.user
         sendResponse(res, 200, false, {},{name,email,role})
@@ -8,3 +8,20 @@ export const user_controller = async (req, res) => {
         sendResponse(res, 500, true, {general:"Something went wrong"},null)
     }
 }
+
+const user_profile_controller = async (req,res) => {
+    try {
+        const { id } = req.user
+        const updates = req.body
+        console.log(id);
+        console.log(updates);
+        
+        
+        sendResponse(res, 200, false, null, null);
+        
+    } catch (error) {
+        sendResponse(res, 500, true, {general:"Something went wrong"},null)
+    }
+    
+}
+export {user_controller,user_profile_controller}
--- backend_express/src/routers/user.router.js
+++ backend_express/src/routers/user.router.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { authenticate_user } from '../middlewares/auth.middleware.js'
-import {user_controller} from '../controllers/user.controller.js'
+import {user_controller, user_profile_controller} from '../controllers/user.controller.js'
 const router = express.Router()
 
 router.get('/myInfo',authenticate_user,user_controller)
+router.put('/profile',authenticate_user,user_profile_controller)
 
 export default router
