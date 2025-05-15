61617152d170d3cda33d241a3a5a706451ecc4ae
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-15 10:03:45 +0500

    add loading page when user refresh the page

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -9,13 +9,22 @@ import VerifyEmail from './pages/verify-email'
 import {useAuth} from './context/Authcontext'
 import NotFound from './components/not-found'
 import Profile from './pages/profile/profile'
+import EntrepreneurProfile from './pages/profile/entrepreneur_profile'
+import InvestorProfile from './pages/profile/investor_profile'
 function App() {
-  const {user} = useAuth()
+  const {user,loading} = useAuth()
   // console.log(user);
   const getDashboardRedirect = () => {
     if (!user) return <Navigate to="/login" />;
     return <Navigate to={user.role === 'investor' ? '/dashboard/investor' : '/dashboard/entrepreneur'} />;
   };
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-50 text-purple-600 text-xl">
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <Routes>
@@ -30,6 +39,8 @@ function App() {
 
         {/* Protected profile Routes */}
         <Route path='/profile' element={<Profile/>} />
+        <Route path='/profile/investor/:id' element={<InvestorProfile/>} />
+        <Route path='/profile/entrepreneur/:id' element={<EntrepreneurProfile/>} />
       </Route>
       <Route path='*' element={<NotFound/>} />
    </Routes>
