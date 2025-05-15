8110c876518952be5c0a34ef8845b0b74f015a92
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-15 13:36:45 +0500

    .

--- backend_express/src/routers/request.router.js
+++ backend_express/src/routers/request.router.js
@@ -3,7 +3,7 @@ import { authenticate_user } from '../middlewares/auth.middleware.js'
 import { get_requests_controller, send_request_to_enterpre_controller } from '../controllers/request.controller.js'
 
 const router = express.Router()
-
+//request
 router.post('/request/:enterpreneurId',authenticate_user,send_request_to_enterpre_controller)
 router.get('/requests',authenticate_user,get_requests_controller)
 
