7220996434976323a7c30a06ce5750a3537b7126
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-05-15 12:47:21 +0500

    create all request route

--- backend_express/src/routers/request.router.js
+++ backend_express/src/routers/request.router.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { authenticate_user } from '../middlewares/auth.middleware.js'
-import { send_request_to_enterpre_controller } from '../controllers/request.controller.js'
+import { get_requests_controller, send_request_to_enterpre_controller } from '../controllers/request.controller.js'
 
 const router = express.Router()
 
 router.post('/request/:enterpreneurId',authenticate_user,send_request_to_enterpre_controller)
+router.get('/requests',authenticate_user,get_requests_controller)
 
 export default router
